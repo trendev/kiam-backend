32c25efef17ae200c0117ad6e0e55b12490d0cba
jsie <julien.sie@gmail.com>
2017-07-09 14:18:30 +0100

    add EntityManagerProducer

---
+++ comptandye_base/comptandye/src/main/java/fr/trendev/comptandye/ejbsessions/producer/EntityManagerProducer.java
@@ -0,0 +1,16 @@
+package fr.trendev.comptandye.ejbsessions.producer;
+
+import javax.enterprise.inject.Produces;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ * Producer for injectable EntityManager
+ *
+ */
+public class EntityManagerProducer {
+
+    @Produces
+    @PersistenceContext(unitName = "DEFAULT_PU")
+    private EntityManager em;
+}
