8174ef06644ea04aef4d9612eab4f46a5aa6fc5b
jsie <julien.sie@gmail.com>
2017-08-13 19:36:59 +0200

    getRealValue

--- comptandye/src/main/java/fr/trendev/comptandye/utils/serialization/UserGroupDeserializer.java
+++ comptandye/src/main/java/fr/trendev/comptandye/utils/serialization/UserGroupDeserializer.java
@@ -14,12 +14,15 @@ import fr.trendev.comptandye.entities.UserGroup;
 import java.io.IOException;
 import java.lang.reflect.Field;
 import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
  *
  * @author jsie
+ * @param <T>
  */
 public class UserGroupDeserializer extends StdDeserializer<UserGroup> {
 
@@ -35,55 +38,39 @@ public class UserGroupDeserializer extends StdDeserializer<UserGroup> {
     public UserGroup deserialize(JsonParser jp, DeserializationContext dc)
             throws IOException, JsonProcessingException {
 
-        Logger.getLogger(
-                UserGroupDeserializer.class.getName()).
+        Logger.getLogger(UserGroupDeserializer.class.getName()).
                 log(Level.INFO, "Deserializing object {0}",
                         UserGroupDeserializer.class);
 
         UserGroup userGroup = new UserGroup();
-
         JsonNode node = jp.readValueAsTree();
 
         node.fields().forEachRemaining(e -> {
             try {
-                Field f = UserGroup.class.getDeclaredField(e.getKey());
+                Field f = userGroup.getClass().getDeclaredField(e.getKey());
                 f.setAccessible(true);
-                Class<?> type = f.getType();
-                Object value = null;
-
-                System.out.println("TYPE: " + type.toGenericString() + " / "
-                        + e.getValue().asText());
-// TODO : use polymorphic method instead...
-                switch (type.toGenericString()) {
-                    case "public final class java.lang.String":
-                        value = e.getValue().asText();
-                        break;
-                    case "public abstract interface java.util.List<E>":
-                        value = Collections.emptyList();
-                        break;
-                    default:
-                        System.err.println("Incorrect type");
-                        break;
-                }
-
                 f.set(userGroup,
-                        value);
+                        this.getRealValue(f.getType(), e));
+
                 f.setAccessible(false);
-            } catch (NoSuchFieldException ex) {
-                Logger.getLogger(UserGroupDeserializer.class.getName()).
-                        log(Level.SEVERE, null, ex);
-            } catch (SecurityException ex) {
-                Logger.getLogger(UserGroupDeserializer.class.getName()).
-                        log(Level.SEVERE, null, ex);
-            } catch (IllegalArgumentException ex) {
-                Logger.getLogger(UserGroupDeserializer.class.getName()).
-                        log(Level.SEVERE, null, ex);
-            } catch (IllegalAccessException ex) {
+            } catch (NoSuchFieldException | SecurityException |
+                    IllegalArgumentException | IllegalAccessException ex) {
                 Logger.getLogger(UserGroupDeserializer.class.getName()).
                         log(Level.SEVERE, null, ex);
             }
         });
+
         return userGroup;
+    }
+
+    private Object getRealValue(Class<?> clazz, Map.Entry<String, JsonNode> e) {
+        if (clazz == String.class) {
+            return e.getValue().asText();
+        }
+        if (clazz == List.class) {
+            return Collections.emptyList();
+        }
+        return null;
     }
 
 }
