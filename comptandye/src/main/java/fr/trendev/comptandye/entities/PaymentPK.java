97fb276eb102e56d03751acb6707922b011bfbad
jsie <julien.sie@gmail.com>
2017-08-21 23:18:51 +0200

    adds constructors and tests to PaymentMode

--- comptandye/src/main/java/fr/trendev/comptandye/entities/PaymentPK.java
+++ comptandye/src/main/java/fr/trendev/comptandye/entities/PaymentPK.java
@@ -56,8 +56,8 @@ public class PaymentPK implements Serializable {
     @Override
     public int hashCode() {
         int hash = 3;
-        hash = 43 * hash + (this.getId() != null ? this.getId().hashCode() : 0);
-        hash = 43 * hash
+        hash = 23 * hash + (this.getId() != null ? this.getId().hashCode() : 0);
+        hash = 23 * hash
                 + (this.getBill() != null ? this.getBill().hashCode() : 0);
         return hash;
     }
