310ebc7e3f6d6868e7c2800473303c4ec5743788
jsie <julien.sie@gmail.com>
2017-08-21 18:40:21 +0200

    adds constructors and tests to Business

---
+++ comptandye/src/test/java/fr/trendev/comptandye/entities/BusinessTest.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.trendev.comptandye.entities;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ *
+ * @author jsie
+ */
+public class BusinessTest {
+
+    public BusinessTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testGetName() {
+
+        Business instance = new Business();
+
+        assert instance.getName() == null;
+
+        String name = "R&D";
+        instance = new Business(name);
+
+        assert name.equals(instance.getName());
+
+    }
+
+}
