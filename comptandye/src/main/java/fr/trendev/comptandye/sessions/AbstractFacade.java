10ed29cf0aadb5268ab320ae6910ecd836d0b510
jsie <julien.sie@gmail.com>
2017-08-24 13:48:09 +0200

    removes findId

--- comptandye/src/main/java/fr/trendev/comptandye/sessions/AbstractFacade.java
+++ comptandye/src/main/java/fr/trendev/comptandye/sessions/AbstractFacade.java
@@ -1,13 +1,10 @@
 package fr.trendev.comptandye.sessions;
 
 import java.util.List;
-import java.util.Optional;
 import javax.persistence.EntityManager;
-import javax.persistence.NoResultException;
-import javax.persistence.Query;
+import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
 
 public abstract class AbstractFacade<E, P> {
 
@@ -31,11 +28,6 @@ public abstract class AbstractFacade<E, P> {
         getEntityManager().remove(getEntityManager().merge(entity));
     }
 
-    public P getIdentifier(E entity) {
-        return (P) getEntityManager().getEntityManagerFactory().
-                getPersistenceUnitUtil().getIdentifier(entity);
-    }
-
     public E find(P id) {
         return getEntityManager().find(entityClass, id);
     }
@@ -49,44 +41,30 @@ public abstract class AbstractFacade<E, P> {
         return em.createQuery(cq).getResultList();
     }
 
-    public List<E> findRange(int startPosition, int size) {
-        return findRange(startPosition, size, null);
+//    public List<E> findRange(int startPosition, int size) {
+//        return findRange(startPosition, size, null);
+//    }
+//
+//    public List<E> findRange(int startPosition, int size, String entityGraph) {
+//        CriteriaQuery cq = getEntityManager().getCriteriaBuilder().createQuery();
+//        cq.select(cq.from(entityClass));
+//        Query q = getEntityManager().createQuery(cq);
+//        q.setMaxResults(size);
+//        q.setFirstResult(startPosition);
+//        if (entityGraph != null) {
+//            q.setHint("javax.persistence.loadgraph", getEntityManager().
+//                    getEntityGraph(entityGraph));
+//        }
+//        return q.getResultList();
+//    }
+    public Long count() {
+
+        EntityManager em = this.getEntityManager();
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<Long> cq = cb.createQuery(Long.class);
+        cq.select(cb.count(cq.from(entityClass)));
+        TypedQuery<Long> q = em.createQuery(cq);
+
+        return q.getSingleResult();
     }
-
-    public List<E> findRange(int startPosition, int size, String entityGraph) {
-        CriteriaQuery cq = getEntityManager().getCriteriaBuilder().createQuery();
-        cq.select(cq.from(entityClass));
-        Query q = getEntityManager().createQuery(cq);
-        q.setMaxResults(size);
-        q.setFirstResult(startPosition);
-        if (entityGraph != null) {
-            q.setHint("javax.persistence.loadgraph", getEntityManager().
-                    getEntityGraph(entityGraph));
-        }
-        return q.getResultList();
-    }
-
-    public int count() {
-        CriteriaQuery cq = getEntityManager().getCriteriaBuilder().createQuery();
-        Root<E> rt = cq.from(entityClass);
-        cq.select(getEntityManager().getCriteriaBuilder().count(rt));
-        Query q = getEntityManager().createQuery(cq);
-        return ((Long) q.getSingleResult()).intValue();
-    }
-
-    public static <E> Optional<E> findOrEmpty(final DaoRetriever<E> retriever) {
-        try {
-            return Optional.of(retriever.retrieve());
-        } catch (NoResultException ex) {
-            //log
-        }
-        return Optional.empty();
-    }
-
-    @FunctionalInterface
-    public interface DaoRetriever<E> {
-
-        E retrieve() throws NoResultException;
-    }
-
 }
