f030257643d242d87fad3c7c5929ec0ec826590c
jsie <julien.sie@gmail.com>
2017-08-26 19:42:00 +0200

    implements PUT method of UserGroup

--- comptandye/src/main/java/fr/trendev/comptandye/services/UserGroupService.java
+++ comptandye/src/main/java/fr/trendev/comptandye/services/UserGroupService.java
@@ -18,6 +18,7 @@ import javax.json.Json;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -31,27 +32,27 @@ import javax.ws.rs.core.Response;
 @Stateless
 @Path("UserGroup")
 public class UserGroupService extends CommonRestService<UserGroup, String> {
-    
+
     @Inject
     UserGroupFacade facade;
-    
+
     @Inject
     Logger logger;
-    
+
     public UserGroupService() {
         super("UserGroup");
     }
-    
+
     @Override
     protected UserGroupFacade getFacade() {
         return facade;
     }
-    
+
     @Override
     protected Logger getLogger() {
         return logger;
     }
-    
+
     @Path("{name}")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
@@ -59,7 +60,7 @@ public class UserGroupService extends CommonRestService<UserGroup, String> {
         logger.log(Level.INFO, "REST request to get UserGroup : {0}", name);
         return super.find(name);
     }
-    
+
     @Path("{name}/userAccounts")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
@@ -73,7 +74,7 @@ public class UserGroupService extends CommonRestService<UserGroup, String> {
                     .orElse(Response.status(Response.Status.NOT_FOUND).entity(
                             Json.createArrayBuilder().build()).build());
         } catch (Exception ex) {
-            
+
             String errmsg = ExceptionHelper.handleException(ex,
                     "Exception occurs providing userAccounts of user-group to administrator");
             getLogger().
@@ -83,7 +84,7 @@ public class UserGroupService extends CommonRestService<UserGroup, String> {
                     build();
         }
     }
-    
+
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
@@ -91,13 +92,13 @@ public class UserGroupService extends CommonRestService<UserGroup, String> {
         getLogger().log(Level.INFO, "Creating UserGroup {0}", entity);
         try {
             getFacade().create(entity);
-            getLogger().log(Level.INFO, "Entity {0} created", entity);
+            getLogger().log(Level.INFO, "UserGroup {0} created", entity);
             return Response.created(new URI("/restapi/UserGroup/" + entity.
                     getName())).
                     entity(entity).
                     build();
         } catch (Exception ex) {
-            
+
             String errmsg = ExceptionHelper.handleException(ex,
                     "Exception occurs creating UserGroup " + entity);
             getLogger().
@@ -107,4 +108,27 @@ public class UserGroupService extends CommonRestService<UserGroup, String> {
                     build();
         }
     }
+
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response update(UserGroup entity) {
+        getLogger().log(Level.INFO, "Updating UserGroup {0}", entity);
+        try {
+            getFacade().edit(entity);
+            getLogger().log(Level.INFO, "UserGroup {0} updated", entity);
+            return Response.ok(entity).
+                    build();
+        } catch (Exception ex) {
+
+            String errmsg = ExceptionHelper.handleException(ex,
+                    "Exception occurs updating UserGroup " + entity);
+            getLogger().
+                    log(Level.WARNING, errmsg);
+            return Response.status(Response.Status.EXPECTATION_FAILED).entity(
+                    Json.createObjectBuilder().add("error", errmsg).build()).
+                    build();
+        }
+    }
+
 }
