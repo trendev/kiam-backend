85b247c13b6b63a189d3d42553c368579074a0c2
jsie <julien.sie@gmail.com>
2017-08-26 16:33:39 +0200

    enables DebugExceptionMapper + logs stackTrace

--- comptandye/src/main/java/fr/trendev/comptandye/utils/exceptions/DebugExceptionMapper.java
+++ comptandye/src/main/java/fr/trendev/comptandye/utils/exceptions/DebugExceptionMapper.java
@@ -5,20 +5,27 @@
  */
 package fr.trendev.comptandye.utils.exceptions;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.json.Json;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
 
 //Uncomment in order to provide this ExceptionMapper
-//@Provider
+@Provider
 public class DebugExceptionMapper implements ExceptionMapper<Exception> {
+
+    private static final Logger logger = Logger.getLogger(
+            DebugExceptionMapper.class.getName());
 
     @Override
     public Response toResponse(Exception ex) {
-        ex.printStackTrace();
+        logger.log(Level.WARNING, ex.getMessage(), ex);
         String errmsg = ExceptionHelper.handleException(ex,
                 "DebugExceptionMapper caught an Exception");
-        return Response.serverError().entity(Json.createObjectBuilder().add(
-                "error", errmsg).build()).build();
+        return Response.status(Response.Status.EXPECTATION_FAILED).entity(Json.
+                createObjectBuilder().add(
+                        "error", errmsg).build()).build();
     }
 }
