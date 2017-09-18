6b35bad9dfd6868f7760922ab10258fe246003d0
jsie <julien.sie@gmail.com>
2017-09-18 18:28:20 +0200

    Comments existing checker code

--- comptandye/src/main/java/fr/trendev/comptandye/services/PaymentService.java
+++ comptandye/src/main/java/fr/trendev/comptandye/services/PaymentService.java
@@ -7,6 +7,7 @@ package fr.trendev.comptandye.services;
 
 import fr.trendev.comptandye.entities.Payment;
 import fr.trendev.comptandye.sessions.PaymentFacade;
+import fr.trendev.comptandye.sessions.PaymentModeFacade;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.ejb.Stateless;
@@ -30,36 +31,39 @@ import javax.ws.rs.core.Response;
 @Stateless
 @Path("Payment")
 public class PaymentService extends AbstractCommonService<Payment, Long> {
-
+    
     @Inject
     PaymentFacade paymentFacade;
-
+    
+    @Inject
+    PaymentModeFacade paymentModeFacade;
+    
     private static final Logger LOG = Logger.getLogger(PaymentService.class.
             getName());
-
+    
     public PaymentService() {
         super(Payment.class);
     }
-
+    
     @Override
     protected Logger getLogger() {
         return LOG;
     }
-
+    
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public Response findAll() {
         LOG.log(Level.INFO, "Providing the Payment list");
         return super.findAll(paymentFacade);
     }
-
+    
     @Path("count")
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public Response count() {
         return super.count(paymentFacade);
     }
-
+    
     @Path("{id}")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
@@ -68,17 +72,17 @@ public class PaymentService extends AbstractCommonService<Payment, Long> {
         LOG.log(Level.INFO, "REST request to get Payment : {0}", id);
         return super.find(paymentFacade, id, refresh);
     }
-
+    
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     public Response post(Payment entity) {
         LOG.log(Level.INFO, "Creating Payment {0}", super.stringify(entity));
-
+        
         return super.post(entity, paymentFacade, e -> {
         });
     }
-
+    
     @PUT
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
@@ -88,11 +92,19 @@ public class PaymentService extends AbstractCommonService<Payment, Long> {
                 e -> {
             e.setAmount(entity.getAmount());
             e.setCurrency(entity.getCurrency());
-            //TODO : avoid to create a new PaymentMode if the provided PaymentMode does not exist
             e.setPaymentMode(entity.getPaymentMode());
+
+//            if (paymentModeFacade.find(entity.getPaymentMode().getName())
+//                    != null) {
+//                e.setPaymentMode(entity.getPaymentMode());
+//            } else {
+//                String msg = "PaymentMode " + entity.getPaymentMode().getName()
+//                        + " doesn't exist !";
+//                throw new IllegalArgumentException(msg);
+//            }
         });
     }
-
+    
     @Path("{id}")
     @DELETE
     public Response delete(@PathParam("id") Long id) {
