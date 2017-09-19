d418c37024200e9b30076e4613a1e2582519ea15
jsie <julien.sie@gmail.com>
2017-09-19 19:28:21 +0200

    replace key word in path by {id} PathParam into ClientService

--- comptandye/src/main/java/fr/trendev/comptandye/services/ClientService.java
+++ comptandye/src/main/java/fr/trendev/comptandye/services/ClientService.java
@@ -22,6 +22,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
@@ -72,10 +73,10 @@ public class ClientService extends AbstractCommonService<Client, ClientPK> {
         return super.count(clientFacade);
     }
 
-    @Path("key")
+    @Path("{id}")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Response find(@QueryParam("id") Long id,
+    public Response find(@PathParam("id") Long id,
             @QueryParam("professional") String professional,
             @QueryParam("refresh") boolean refresh) {
         ClientPK pk = new ClientPK(id, professional);
@@ -136,10 +137,10 @@ public class ClientService extends AbstractCommonService<Client, ClientPK> {
         });
     }
 
-    @Path("key")
+    @Path("{id}")
     @DELETE
     public Response delete(@Context SecurityContext sec,
-            @QueryParam("id") Long id,
+            @PathParam("id") Long id,
             @QueryParam("professional") String professional) {
 
         ClientPK pk = new ClientPK(id, this.getProEmail(sec,
@@ -161,10 +162,10 @@ public class ClientService extends AbstractCommonService<Client, ClientPK> {
         });
     }
 
-    @Path("clientBills/key")
+    @Path("{id}/clientBills")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getClients(@QueryParam("id") Long id,
+    public Response getClients(@PathParam("id") Long id,
             @QueryParam("professional") String professional) {
         ClientPK pk = new ClientPK(id, professional);
         LOG.log(Level.INFO,
@@ -174,10 +175,10 @@ public class ClientService extends AbstractCommonService<Client, ClientPK> {
                 pk, Client::getClientBills);
     }
 
-    @Path("collectiveGroups/key")
+    @Path("{id}/collectiveGroups")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getCollectiveGroups(@QueryParam("id") Long id,
+    public Response getCollectiveGroups(@PathParam("id") Long id,
             @QueryParam("professional") String professional) {
         ClientPK pk = new ClientPK(id, professional);
         LOG.log(Level.INFO,
@@ -187,10 +188,10 @@ public class ClientService extends AbstractCommonService<Client, ClientPK> {
                 pk, Client::getCollectiveGroups);
     }
 
-    @Path("categories/key")
+    @Path("{id}/categories")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getCategories(@QueryParam("id") Long id,
+    public Response getCategories(@PathParam("id") Long id,
             @QueryParam("professional") String professional) {
         ClientPK pk = new ClientPK(id, professional);
         LOG.log(Level.INFO,
