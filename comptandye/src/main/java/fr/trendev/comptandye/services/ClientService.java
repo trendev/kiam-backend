45fada3be26008f0528c9f627e2057225415abee
jsie <julien.sie@gmail.com>
2017-09-19 17:56:37 +0200

    inits ClientService

---
+++ comptandye/src/main/java/fr/trendev/comptandye/services/ClientService.java
@@ -0,0 +1,202 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.trendev.comptandye.services;
+
+import fr.trendev.comptandye.entities.Client;
+import fr.trendev.comptandye.entities.ClientPK;
+import fr.trendev.comptandye.entities.Professional;
+import fr.trendev.comptandye.sessions.AbstractFacade;
+import fr.trendev.comptandye.sessions.ClientBillFacade;
+import fr.trendev.comptandye.sessions.ClientFacade;
+import fr.trendev.comptandye.sessions.ProfessionalFacade;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.SecurityContext;
+
+/**
+ *
+ * @author jsie
+ */
+@Stateless
+@Path("Client")
+public class ClientService extends AbstractCommonService<Client, ClientPK> {
+
+    @Inject
+    ClientFacade clientFacade;
+
+    @Inject
+    ProfessionalFacade professionalFacade;
+
+    @Inject
+    ClientBillFacade clientBillFacade;
+
+    private static final Logger LOG = Logger.getLogger(ClientService.class.
+            getName());
+
+    public ClientService() {
+        super(Client.class);
+    }
+
+    @Override
+    protected Logger getLogger() {
+        return LOG;
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response findAll() {
+        LOG.log(Level.INFO, "Providing the Client list");
+        return super.findAll(clientFacade);
+    }
+
+    @Path("count")
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response count() {
+        return super.count(clientFacade);
+    }
+
+    @Path("key")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response find(@QueryParam("id") Long id,
+            @QueryParam("professional") String professional,
+            @QueryParam("refresh") boolean refresh) {
+        ClientPK pk = new ClientPK(id, professional);
+        LOG.log(Level.INFO, "REST request to get Client : {0}",
+                clientFacade.
+                        prettyPrintPK(
+                                pk));
+        return super.find(clientFacade, pk, refresh);
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response post(@Context SecurityContext sec, Client entity,
+            @QueryParam("professional") String professional) {
+
+        String email = this.getProEmail(sec, professional);
+
+        return super.<Professional, String>post(entity, email,
+                AbstractFacade::prettyPrintPK,
+                Professional.class,
+                clientFacade, professionalFacade,
+                Client::setProfessional,
+                Professional::getClients, e -> {
+        });
+
+    }
+
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response put(@Context SecurityContext sec, Client entity,
+            @QueryParam("professional") String professional) {
+
+        ClientPK pk = new ClientPK(entity.getId(), this.
+                getProEmail(sec,
+                        professional));
+
+        LOG.log(Level.INFO, "Updating Client {0}",
+                clientFacade.
+                        prettyPrintPK(pk));
+        return super.put(entity, clientFacade, pk, e -> {
+            /**
+             * Will automatically ignore the id of the provided object : avoid
+             * to hack another object swapping the current saved (or not) object
+             * by an existing one.
+             */
+            entity.getCustomerDetails().setId(e.getCustomerDetails().getId());
+            entity.getAddress().setId(e.getAddress().getId());
+            entity.getSocialNetworkAccounts().setId(
+                    e.getSocialNetworkAccounts().getId());
+
+            e.setCustomerDetails(entity.getCustomerDetails());
+            e.setAddress(entity.getAddress());
+            e.setSocialNetworkAccounts(entity.getSocialNetworkAccounts());
+
+            e.setEmail(entity.getEmail());
+        });
+    }
+
+    @Path("key")
+    @DELETE
+    public Response delete(@Context SecurityContext sec,
+            @QueryParam("id") Long id,
+            @QueryParam("professional") String professional) {
+
+        ClientPK pk = new ClientPK(id, this.getProEmail(sec,
+                professional));
+
+        LOG.log(Level.INFO, "Deleting Client {0}",
+                clientFacade.
+                        prettyPrintPK(pk));
+        return super.delete(clientFacade, pk,
+                e -> {
+            /**
+             * If there is an existing relationship between Client and
+             * ClientBill, for compliance purposes, the relationship cannot be
+             * broken and the Client cannot be deleted.
+             */
+            e.getProfessional().getClients().remove(e);
+            e.getCollectiveGroups().forEach(cg -> cg.getClients().remove(e));
+            e.getCategories().forEach(ct -> ct.getClients().remove(e));
+        });
+    }
+
+    @Path("clientBills/key")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getClients(@QueryParam("id") Long id,
+            @QueryParam("professional") String professional) {
+        ClientPK pk = new ClientPK(id, professional);
+        LOG.log(Level.INFO,
+                "REST request to get ClientBills of Client : {0}",
+                clientFacade.prettyPrintPK(pk));
+        return super.provideRelation(clientFacade,
+                pk, Client::getClientBills);
+    }
+
+    @Path("collectiveGroups/key")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getCollectiveGroups(@QueryParam("id") Long id,
+            @QueryParam("professional") String professional) {
+        ClientPK pk = new ClientPK(id, professional);
+        LOG.log(Level.INFO,
+                "REST request to get CollectiveGroups of Client : {0}",
+                clientFacade.prettyPrintPK(pk));
+        return super.provideRelation(clientFacade,
+                pk, Client::getCollectiveGroups);
+    }
+
+    @Path("categories/key")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getCategories(@QueryParam("id") Long id,
+            @QueryParam("professional") String professional) {
+        ClientPK pk = new ClientPK(id, professional);
+        LOG.log(Level.INFO,
+                "REST request to get Categories of Client : {0}",
+                clientFacade.prettyPrintPK(pk));
+        return super.provideRelation(clientFacade,
+                pk, Client::getCategories);
+    }
+}
