83eba1671143f6c4da8708aba69ab716e0540d37
jsie <julien.sie@gmail.com>
2017-09-21 17:59:53 +0200

    remove jsonignore annotation in IndividualBill

--- comptandye/src/main/java/fr/trendev/comptandye/entities/CollectiveGroupPK.java
+++ comptandye/src/main/java/fr/trendev/comptandye/entities/CollectiveGroupPK.java
@@ -57,8 +57,8 @@ public class CollectiveGroupPK implements Serializable {
     @Override
     public int hashCode() {
         int hash = 7;
-        hash = 67 * hash + (this.getId() != null ? this.getId().hashCode() : 0);
-        hash = 67 * hash + (this.getProfessional() != null ? this.
+        hash = 97 * hash + (this.getId() != null ? this.getId().hashCode() : 0);
+        hash = 97 * hash + (this.getProfessional() != null ? this.
                 getProfessional().hashCode() : 0);
         return hash;
     }
