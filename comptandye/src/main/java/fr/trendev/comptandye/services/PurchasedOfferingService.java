be48253104439f0a48bdc0e5cbe07ba68b9ba74b
jsie <julien.sie@gmail.com>
2017-09-26 18:37:25 +0200

    implements and tests PurchasedOfferingService

--- comptandye/src/main/java/fr/trendev/comptandye/services/PurchasedOfferingService.java
+++ comptandye/src/main/java/fr/trendev/comptandye/services/PurchasedOfferingService.java
@@ -5,8 +5,12 @@
  */
 package fr.trendev.comptandye.services;
 
+import fr.trendev.comptandye.entities.OfferingPK;
 import fr.trendev.comptandye.entities.PurchasedOffering;
 import fr.trendev.comptandye.sessions.PurchasedOfferingFacade;
+import fr.trendev.comptandye.visitors.ProvideOfferingFacadeVisitor;
+import java.util.Optional;
+import java.util.function.Function;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.ejb.Stateless;
@@ -20,6 +24,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -30,37 +35,40 @@ import javax.ws.rs.core.Response;
 @Stateless
 @Path("PurchasedOffering")
 public class PurchasedOfferingService extends AbstractCommonService<PurchasedOffering, Long> {
-    
+
     @Inject
     PurchasedOfferingFacade purchasedOfferingFacade;
-    
+
+    @Inject
+    ProvideOfferingFacadeVisitor visitor;
+
     private static final Logger LOG = Logger.getLogger(
             PurchasedOfferingService.class.
                     getName());
-    
+
     public PurchasedOfferingService() {
         super(PurchasedOffering.class);
     }
-    
+
     @Override
     protected Logger getLogger() {
         return LOG;
     }
-    
+
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public Response findAll() {
         LOG.log(Level.INFO, "Providing the PurchasedOffering list");
         return super.findAll(purchasedOfferingFacade);
     }
-    
+
     @Path("count")
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public Response count() {
         return super.count(purchasedOfferingFacade);
     }
-    
+
     @Path("{id}")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
@@ -69,30 +77,60 @@ public class PurchasedOfferingService extends AbstractCommonService<PurchasedOff
         LOG.log(Level.INFO, "REST request to get PurchasedOffering : {0}", id);
         return super.find(purchasedOfferingFacade, id, refresh);
     }
-    
+
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public Response post(PurchasedOffering entity) {
+    public Response post(PurchasedOffering entity,
+            @QueryParam("professional") String professional) {
         LOG.log(Level.INFO, "Creating PurchasedOffering {0}", super.stringify(
                 entity));
-        
+
+        if (professional == null) {
+            throw new WebApplicationException(
+                    "Profession email is not provided !");
+        }
+
         return super.post(entity, purchasedOfferingFacade, e -> {
+
+            if (e.getOffering() == null) {
+                throw new WebApplicationException("No Offering provided !");
+            }
+
+            e.setOffering(Optional.ofNullable(e.getOffering().accept(visitor).
+                    find(new OfferingPK(e.getOffering().getId(), professional)))
+                    .map(Function.identity())
+                    .orElseThrow(() -> new WebApplicationException()));
         });
     }
-    
+
     @PUT
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public Response put(PurchasedOffering entity) {
+    public Response put(PurchasedOffering entity,
+            @QueryParam("professional") String professional) {
         LOG.log(Level.INFO, "Updating PurchasedOffering {0}", entity.getId());
+
+        if (professional == null) {
+            throw new WebApplicationException(
+                    "Profession email is not provided !");
+        }
+
         return super.put(entity, purchasedOfferingFacade, entity.getId(),
                 e -> {
+
+            if (e.getOffering() == null) {
+                throw new WebApplicationException("No Offering provided !");
+            }
+
             e.setQty(entity.getQty());
-            e.setOffering(entity.getOffering());
+            e.setOffering(Optional.ofNullable(e.getOffering().accept(visitor).
+                    find(new OfferingPK(e.getOffering().getId(), professional)))
+                    .map(Function.identity())
+                    .orElseThrow(() -> new WebApplicationException()));
         });
     }
-    
+
     @Path("{id}")
     @DELETE
     public Response delete(@PathParam("id") Long id) {
