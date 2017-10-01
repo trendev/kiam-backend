7ff828f8b2ffe60efc3b6ea5bde1d0945c60d32e
jsie <julien.sie@gmail.com>
2017-10-01 17:51:46 +0200

    implements NotFoundExceptionMapper

---
+++ comptandye/src/main/java/fr/trendev/comptandye/utils/exceptions/NotFoundExceptionMapper.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.trendev.comptandye.utils.exceptions;
+
+import javax.ws.rs.NotFoundException;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class NotFoundExceptionMapper implements
+        ExceptionMapper<NotFoundException> {
+
+    @Override
+    public Response toResponse(NotFoundException ex) {
+        return ex.getResponse();
+    }
+}
