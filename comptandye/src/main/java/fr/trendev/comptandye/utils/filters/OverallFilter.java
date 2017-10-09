dd56037f6fe62df058cbc9d3431d4868353cc46a
jsie <julien.sie@gmail.com>
2017-10-09 19:53:36 +0200

    adds an Overall Filter

---
+++ comptandye/src/main/java/fr/trendev/comptandye/utils/filters/OverallFilter.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.trendev.comptandye.utils.filters;
+
+import java.io.IOException;
+import java.security.Principal;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author jsie
+ */
+public class OverallFilter implements Filter {
+
+    private static final Logger LOG = Logger.getLogger(OverallFilter.class.
+            getName());
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        LOG.log(Level.INFO, "OverallFilter: init in progress...");
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response,
+            FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse resp = (HttpServletResponse) response;
+
+        Principal user = req.getUserPrincipal();
+
+        LOG.log(Level.INFO, "{3} / [{1}] has requested {2} {0}",
+                new Object[]{req.getRequestURL(), (user != null) ? user.
+                    getName() : "an ANONYMOUS user", req.getMethod(), req.
+                    getRemoteAddr()});
+
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+        LOG.log(Level.INFO, "OverallFilter: destroyed...");
+    }
+
+}
