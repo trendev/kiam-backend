52a6dde9f9f60e533cad0bfa5f05e0540493c995
jsie <julien.sie@gmail.com>
2017-10-11 18:57:37 +0200

    add a fack linechart for connections histo

--- comptandye/src/main/java/fr/trendev/comptandye/beans/LoggedInUsersBean.java
+++ comptandye/src/main/java/fr/trendev/comptandye/beans/LoggedInUsersBean.java
@@ -7,16 +7,25 @@ package fr.trendev.comptandye.beans;
 
 import fr.trendev.comptandye.sessions.UserAccountFacade;
 import java.io.Serializable;
+import java.util.Calendar;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 import java.util.TreeMap;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.servlet.http.HttpSession;
+import org.primefaces.model.chart.Axis;
+import org.primefaces.model.chart.AxisType;
+import org.primefaces.model.chart.CategoryAxis;
+import org.primefaces.model.chart.ChartSeries;
+import org.primefaces.model.chart.LegendPlacement;
+import org.primefaces.model.chart.LineChartModel;
 import org.primefaces.model.chart.PieChartModel;
 
 /**
@@ -40,6 +49,8 @@ public class LoggedInUsersBean implements Serializable {
     private List<LoggedInUser> loggedInUsers;
 
     private PieChartModel userTypes;
+
+    private LineChartModel connections;
 
     private static final Logger LOG = Logger.getLogger(LoggedInUsersBean.class.
             getName());
@@ -87,10 +98,52 @@ public class LoggedInUsersBean implements Serializable {
         });
 
         userTypes = new PieChartModel(map);
-        userTypes.setTitle("Users Set");
-        userTypes.setLegendPosition("e");
+        userTypes.setTitle("Distribution of users");
+        userTypes.setLegendPosition("nw");
         userTypes.setShowDataLabels(true);
         return userTypes;
+    }
+
+    public LineChartModel getConnections() {
+        connections = this.initConnectionModel();
+        connections.setTitle("Connections");
+        connections.setLegendPosition("e");
+        connections.setLegendPlacement(LegendPlacement.OUTSIDEGRID);
+        connections.getAxes().put(AxisType.X, new CategoryAxis("Time"));
+        Axis yAxis = connections.getAxis(AxisType.Y);
+        yAxis.setLabel("Number of Connections");
+        yAxis.setMin(0);
+        yAxis.setMax(25);
+
+        return connections;
+    }
+
+    private LineChartModel initConnectionModel() {
+        LineChartModel model = new LineChartModel();
+
+        int timeout = 1800;//30 min = 1800s
+        int stripe = 6;//cuts in 5 min stripe
+
+        List<String> types = this.loggedInUsers.stream().map(
+                LoggedInUser::getType).distinct().
+                collect(Collectors.toList());
+
+        types.forEach((type) -> {
+            ChartSeries serie = new ChartSeries();
+            serie.setLabel(type);
+
+            Calendar cal = Calendar.getInstance();
+            Random r = new Random();
+            for (int i = 12; i > 0; i--) {
+                cal.add(Calendar.MINUTE, -5 * i);
+                serie.set("t(" + (-5 * i) + ")", r.nextInt(20));
+            }
+
+            model.addSeries(serie);
+
+        });
+
+        return model;
     }
 
     public static class LoggedInUser {
