e94cd44bf19d9da50598f1bf77e21ce41c99f6dd
jsie <julien.sie@gmail.com>
2017-10-20 19:56:38 +0200

    adds XSRF-TOKEN Cookie + set it as attribute in the Session

--- comptandye/src/main/java/fr/trendev/comptandye/utils/filters/OverallFilter.java
+++ comptandye/src/main/java/fr/trendev/comptandye/utils/filters/OverallFilter.java
@@ -6,6 +6,7 @@
 package fr.trendev.comptandye.utils.filters;
 
 import fr.trendev.comptandye.beans.ActiveSessionTracker;
+import fr.trendev.comptandye.utils.PasswordGenerator;
 import java.io.IOException;
 import java.security.Principal;
 import java.util.logging.Level;
@@ -17,53 +18,65 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  *
  * @author jsie
  */
 public class OverallFilter implements Filter {
-
+    
     @Inject
     ActiveSessionTracker tracker;
-
+    
     private static final Logger LOG = Logger.getLogger(OverallFilter.class.
             getName());
-
+    
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
         LOG.log(Level.INFO, "OverallFilter: init in progress...");
     }
-
+    
     @Override
     public void doFilter(ServletRequest request, ServletResponse response,
             FilterChain chain) throws IOException, ServletException {
         HttpServletRequest req = (HttpServletRequest) request;
-
+        HttpServletResponse rsp = (HttpServletResponse) response;
+        
         Principal user = req.getUserPrincipal();
-
+        
+        HttpSession session = req.getSession();
+        
         LOG.log(Level.INFO, "{3} / [{1}] has requested {2} {0}",
                 new Object[]{req.getRequestURL(), (user != null) ? user.
                     getName() : "an ANONYMOUS user", req.getMethod(), req.
                     getRemoteAddr()});
-
-        if (user != null && req.getSession() != null
-                && !tracker.contains(user.getName(), req.getSession())) {
-            tracker.put(user.getName(), req.getSession());
+        
+        if (user != null && session != null
+                && !tracker.contains(user.getName(), session)) {
+            tracker.put(user.getName(), session);
             int count = tracker.count(user.getName());
             LOG.log(Level.INFO,
                     "New session {0} added to ActiveSessionTracker. [{1}] has now {2} active session{3}",
-                    new Object[]{req.getSession().getId(),
+                    new Object[]{session.getId(),
                         user.getName(), count, count > 1 ? "s" : ""});
+            
+            String xsrfToken = PasswordGenerator.autoGenerate(128);
+            Cookie c = new Cookie("XSRF-TOKEN", xsrfToken);
+            session.setAttribute("XSRF-TOKEN", xsrfToken);
+            rsp.addCookie(c);
+            
         }
-
+        
         chain.doFilter(request, response);
     }
-
+    
     @Override
     public void destroy() {
         LOG.log(Level.INFO, "OverallFilter: destroyed...");
     }
-
+    
 }
