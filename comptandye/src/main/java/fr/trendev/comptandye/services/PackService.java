323b82db371645256f785b4956622afc3168fce2
jsie <julien.sie@gmail.com>
2017-10-29 22:02:05 +0100

    avoid to add a Pack in itself (-> stack overflow)

--- comptandye/src/main/java/fr/trendev/comptandye/services/PackService.java
+++ comptandye/src/main/java/fr/trendev/comptandye/services/PackService.java
@@ -20,6 +20,7 @@ import java.util.logging.Logger;
 import javax.annotation.security.RolesAllowed;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
+import javax.json.Json;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -222,6 +223,18 @@ public class PackService extends AbstractCommonService<Pack, OfferingPK> {
             @PathParam("packid") Long packid,
             @PathParam("offeringid") Long offeringid,
             @QueryParam("professional") String professional) {
+
+        //avoid to add a pack in itself (infinite loop)
+        if (packid == offeringid) {
+            LOG.log(Level.WARNING, "Pack {0} cannot be added in itself", packid);
+            return Response.status(Response.Status.EXPECTATION_FAILED)
+                    .entity(Json.createObjectBuilder()
+                            .add("error", "Pack " + packid
+                                    + " cannot be added in itself")
+                            .build()
+                    )
+                    .build();
+        }
 
         OfferingPK packPK = new OfferingPK(packid, this.getProEmail(sec,
                 professional));
