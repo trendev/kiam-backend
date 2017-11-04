a96575c8450e714ddf9c4977a72583071e4e0f79
jsie <julien.sie@gmail.com>
2017-11-04 16:27:28 +0100

    ReloadWebapp servlet + autocomplete off + min/max scale for mobile users

---
+++ comptandye/src/main/java/fr/trendev/comptandye/utils/servlets/ReloadWebApp.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.trendev.comptandye.utils.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author jsie
+ */
+public class ReloadWebApp extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request,
+            HttpServletResponse response)
+            throws ServletException, IOException {
+        request.getRequestDispatcher("/index.html").include(
+                request,
+                response);
+    }
+
+}
