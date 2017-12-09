1fbb22a2ac4e66f8ad85f1163bedabbf7c1d0a5e
jsie <julien.sie@gmail.com>
2017-12-09 21:37:37 +0100

    enable database reset for localhost usage (no docker)

--- comptandye/src/main/java/fr/trendev/comptandye/beans/DemoConfigureBean.java
+++ comptandye/src/main/java/fr/trendev/comptandye/beans/DemoConfigureBean.java
@@ -49,7 +49,9 @@ import java.util.logging.Logger;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import javax.annotation.PostConstruct;
+import javax.ejb.Startup;
 import javax.inject.Inject;
+import javax.inject.Singleton;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.ws.rs.WebApplicationException;
@@ -58,8 +60,8 @@ import javax.ws.rs.WebApplicationException;
  *
  * @author jsie
  */
-//@Singleton
-//@Startup
+@Singleton
+@Startup
 public class DemoConfigureBean implements Serializable {
 
     private final Logger LOG = Logger.getLogger(
