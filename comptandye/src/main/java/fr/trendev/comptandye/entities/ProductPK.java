3203b22e939525c68efd7c57cda7a035d6b7ff86
jsie <julien.sie@gmail.com>
2018-05-17 11:25:27 +0200

    test cltype in Individual

--- comptandye/src/main/java/fr/trendev/comptandye/entities/ProductPK.java
+++ comptandye/src/main/java/fr/trendev/comptandye/entities/ProductPK.java
@@ -57,10 +57,10 @@ public class ProductPK implements Serializable {
 
     @Override
     public int hashCode() {
-        int hash = 7;
-        hash = 67 * hash + (this.getProfessional() != null ? this.
+        int hash = 5;
+        hash = 97 * hash + (this.getProfessional() != null ? this.
                 getProfessional().hashCode() : 0);
-        hash = 67 * hash + (this.getProductReference() != null ? this.
+        hash = 97 * hash + (this.getProductReference() != null ? this.
                 getProductReference().hashCode() : 0);
         return hash;
     }
