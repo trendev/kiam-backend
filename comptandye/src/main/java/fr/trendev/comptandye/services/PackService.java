a02ec7685b7e11b811479c6e813646c1aa31e278
jsie <julien.sie@gmail.com>
2018-07-02 12:30:35 +0200

    move buildModelOfferings() + private methods in new service

--- comptandye/src/main/java/fr/trendev/comptandye/services/PackService.java
+++ comptandye/src/main/java/fr/trendev/comptandye/services/PackService.java
@@ -14,23 +14,14 @@ import fr.trendev.comptandye.entities.Sale;
 import fr.trendev.comptandye.entities.Service;
 import fr.trendev.comptandye.sessions.AbstractFacade;
 import fr.trendev.comptandye.utils.AssociationManagementEnum;
-import fr.trendev.comptandye.utils.exceptions.ExceptionHelper;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URI;
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import java.util.TreeMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
 import javax.annotation.security.RolesAllowed;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
-import javax.json.Json;
 import javax.ws.rs.BadRequestException;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
@@ -45,7 +36,6 @@ import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.core.SecurityContext;
 
 /**
@@ -177,7 +167,7 @@ public class PackService extends AbstractOfferingService<Pack> {
                 List<Offering> offerings = entity.getOfferings().stream()
                         .map(o -> {
                             Offering _o = checkOfferingIntegrity(o, email);
-                            this.checkPackCyclicDependencies(e, _o);
+                            this.controlPackCyclicDependencies(e, _o);
                             _o.getParentPacks().add(e);
                             return _o;
                         })
@@ -284,13 +274,13 @@ public class PackService extends AbstractOfferingService<Pack> {
                 packFacade,
                 offeringPK, Pack.class,
                 (p, o) -> {
-            this.checkPackCyclicDependencies(p, o);
+            this.controlPackCyclicDependencies(p, o);
             return p.getOfferings().add(o) && o.getParentPacks().add(p);
         }
         );
     }
 
-    private void checkPackCyclicDependencies(Pack container, Offering offering) {
+    private void controlPackCyclicDependencies(Pack container, Offering offering) {
         //avoid to add a pack in itself (infinite loop)
         if (container.getId() == offering.getId()) {
             throw new BadRequestException("Pack " + container.getId()
@@ -397,127 +387,4 @@ public class PackService extends AbstractOfferingService<Pack> {
         return super.getParentPacks(pk);
     }
 
-    @Path("buildModelOfferings")
-    @POST
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response buildModelOfferings(@Context SecurityContext sec,
-            @QueryParam("professional") String professional) {
-
-        String email = super.getProEmail(sec, professional);
-        LOG.log(Level.INFO,
-                "Generating pre-build Offerings for the professional [{0}]",
-                email);
-
-        return Optional.ofNullable(professionalFacade.find(email))
-                .map(pro -> {
-                    try {
-                        Map<String, Offering> services = this.
-                                importServices(pro);
-                        List<Offering> packs = this.importPacks(pro, services);
-
-                        //concat pack and services
-                        packs.addAll(services.values());
-
-                        //avoid to return pro.getOffering(): can be outdated offering!
-                        return Response.created(
-                                new URI("Professional/offerings"))
-                                .entity(packs)
-                                .build();
-                    } catch (Exception ex) {
-                        String errmsg = ExceptionHelper.handleException(ex,
-                                "Error occurs Generating pre-build Offerings for the professional "
-                                + email);
-                        getLogger().log(Level.WARNING, errmsg, ex);
-                        throw new WebApplicationException(errmsg, ex);
-                    }
-                })
-                .orElse(Response.status(Status.NOT_FOUND).entity(Json.
-                        createObjectBuilder()
-                        .add("error", "Professional " + email + " not found")
-                        .build())
-                        .build());
-    }
-
-    /**
-     * Reads and imports Services from a JSON file
-     *
-     * @param pro the future owner of the services
-     * @return a Map of the newly created Services
-     * @throws IOException if an error occurs reading/parsing the file
-     */
-    private Map<String, Offering> importServices(Professional pro) throws
-            IOException {
-
-        ClassLoader classloader = Thread.currentThread().
-                getContextClassLoader();
-        InputStream is = classloader.getResourceAsStream(
-                "json/services_hairdressing.json");
-
-        Map<String, Offering> map = new TreeMap<>();
-
-        Arrays.asList(om.readValue(is, Service[].class)).stream()
-                .map(s -> {
-                    s.setProfessional(pro);
-                    pro.getOfferings().add(s);
-                    return s;
-                })
-                .forEach(s -> {
-                    serviceFacade.create(s);
-                    //map the managed entity
-                    map.put(s.getName(), s);
-                });
-
-        is.close();
-
-        return map;
-    }
-
-    /**
-     * Reads and imports Packs from a JSON file
-     *
-     * @param pro the future owner of the Packs
-     * @param services the Services Map, previously imported
-     * @throws IOException if an error occurs reading/parsing the file
-     */
-    private List<Offering> importPacks(Professional pro,
-            Map<String, Offering> services)
-            throws IOException {
-        ClassLoader classloader = Thread.currentThread().
-                getContextClassLoader();
-        InputStream is = classloader.getResourceAsStream(
-                "json/packs_hairdressing.json");
-
-        List<Offering> packs = Arrays.asList(om.readValue(is, Pack[].class)).
-                stream()
-                .map(p -> {
-                    List<Offering> offerings = p.getOfferings().stream()
-                            //use the managed entity instead of the provided offering
-                            .map(o -> Optional.ofNullable(
-                                    services.get(o.getName()))
-                                    .map(_o -> {
-                                        //update the link between the offering and the pack
-                                        _o.getParentPacks().add(p);
-                                        return _o;
-                                    })
-                                    //occurs if json file contains errors
-                                    .orElseThrow(() ->
-                                            new WebApplicationException(
-                                                    "Service " + o.getName()
-                                                    + " not found !"))
-                            )
-                            .collect(Collectors.toList());
-
-                    p.setOfferings(offerings);
-
-                    p.setProfessional(pro);
-                    pro.getOfferings().add(p);
-                    packFacade.create(p);
-                    return p;
-                })
-                .collect(Collectors.toList());
-
-        is.close();
-
-        return packs;
-    }
 }
