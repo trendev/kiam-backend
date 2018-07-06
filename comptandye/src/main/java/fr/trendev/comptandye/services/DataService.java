eab9c7824790e643198e55e68e2d6838e80bd3c9
jsie <julien.sie@gmail.com>
2018-07-06 18:14:22 +0200

    implement/test controlJson

--- comptandye/src/main/java/fr/trendev/comptandye/services/DataService.java
+++ comptandye/src/main/java/fr/trendev/comptandye/services/DataService.java
@@ -7,14 +7,18 @@ package fr.trendev.comptandye.services;
 
 import fr.trendev.comptandye.utils.AuthenticationSecurityUtils;
 import fr.trendev.comptandye.utils.backups.JsonProfessionalExporter;
+import fr.trendev.comptandye.utils.backups.JsonProfessionalImporter;
 import fr.trendev.comptandye.utils.exceptions.ExceptionHandler;
+import java.io.InputStream;
 import java.util.concurrent.CompletableFuture;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.annotation.security.RolesAllowed;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
@@ -32,19 +36,22 @@ import javax.ws.rs.core.SecurityContext;
 @Path("Data")
 @RolesAllowed({"Administrator", "Professional"})
 public class DataService {
-
+    
     private final Logger LOG = Logger.getLogger(DataService.class.
             getName());
-
+    
     @Inject
     private JsonProfessionalExporter jsonProExport;
-
+    
+    @Inject
+    private JsonProfessionalImporter jsonProImport;
+    
     @Inject
     private AuthenticationSecurityUtils authenticationSecurityUtils;
-
+    
     @Inject
     protected ExceptionHandler exceptionHandler;
-
+    
     @GET
     @Path("export/json")
     @Produces(MediaType.APPLICATION_JSON)
@@ -52,17 +59,45 @@ public class DataService {
             @Context SecurityContext sec,
             @QueryParam("f") boolean asFile,
             @QueryParam("professional") String professional) {
-
+        
         String email = authenticationSecurityUtils.
                 getProEmail(sec, professional);
-
+        
         LOG.log(Level.INFO, "Exporting data of Professional " + email
                 + (asFile ? " into a json file" : ""));
-
+        
         CompletableFuture
                 .supplyAsync(() -> jsonProExport.export(email, asFile))
                 .thenApply(result -> ar.resume(result))
                 .exceptionally(e -> ar.resume(exceptionHandler.handle(e)));
     }
 
+    /**
+     * Controls if the JsonProfessionalBackup provided as is valid or not.
+     *
+     * @param ar the Asynchronous Response
+     * @param sec the security context
+     * @param sjpb serialized JsonProfessionalBackup
+     */
+    @POST
+    @Path("control/json")
+    @Consumes(MediaType.TEXT_PLAIN)
+    @Produces(MediaType.APPLICATION_JSON)
+    public void controlJson(@Suspended final AsyncResponse ar,
+            @Context SecurityContext sec,
+            InputStream is) {
+        
+        LOG.log(Level.WARNING, "Receiving a backup...");
+        
+        authenticationSecurityUtils.getProfessionalEmailFromSecurityContext(sec)
+                .ifPresent(email -> LOG.log(Level.INFO, email
+                        + " is controlling a backup..."));
+        
+        CompletableFuture
+                .supplyAsync(() -> jsonProImport.builResponse(is))
+                .thenApply(result -> ar.resume(result))
+                .exceptionally(e -> ar.resume(exceptionHandler.handle(e)));
+        
+    }
+    
 }
