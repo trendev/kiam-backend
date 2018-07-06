106a98d364d3dca002c6cc33fab43f5c60ff3ca7
jsie <julien.sie@gmail.com>
2018-07-06 18:09:14 +0200

    improve the JsonProfessionalBackup with init()/ isValid()

--- comptandye/src/main/java/fr/trendev/comptandye/utils/backups/JsonProfessionalBackup.java
+++ comptandye/src/main/java/fr/trendev/comptandye/utils/backups/JsonProfessionalBackup.java
@@ -5,9 +5,13 @@
  */
 package fr.trendev.comptandye.utils.backups;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import fr.trendev.comptandye.entities.Professional;
 import fr.trendev.comptandye.utils.EncryptionSalt;
 import fr.trendev.comptandye.utils.EncryptionUtils;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ws.rs.WebApplicationException;
 
 /**
  *
@@ -19,17 +23,19 @@ public class JsonProfessionalBackup {
     private String checksum;
     private Professional professional;
 
+    private final Logger LOG = Logger.getLogger(JsonProfessionalBackup.class.
+            getName());
+
     public JsonProfessionalBackup() {
     }
 
-    public JsonProfessionalBackup(Professional pro,
-            EncryptionUtils encryptionUtils,
-            String json) {
+    public JsonProfessionalBackup(Professional professional) {
         this.timestamp = System.currentTimeMillis();
-        String salt = pro.getEmail() + EncryptionSalt.PRAESTAT + timestamp;
-        this.checksum = encryptionUtils.encrypt_SHA512_base64(
-                json + salt);
-        this.professional = pro;
+        this.professional = professional;
+    }
+
+    private String getSalt() {
+        return professional.getEmail() + EncryptionSalt.PRAESTAT + timestamp;
     }
 
     public long getTimestamp() {
@@ -54,6 +60,32 @@ public class JsonProfessionalBackup {
 
     public void setProfessional(Professional professional) {
         this.professional = professional;
+    }
+
+    public void initChecksum(EncryptionUtils encryptionUtils,
+            String json) {
+        this.checksum = encryptionUtils.encrypt_SHA512_base64(
+                json + getSalt());
+        LOG.
+                log(Level.INFO, "Checksum initialized with value : "
+                        + this.checksum);
+    }
+
+    public boolean isValid(ObjectMapper om, EncryptionUtils encryptionUtils) {
+        try {
+            LOG.log(Level.INFO, "Validating backup of Professional "
+                    + professional.getEmail());
+            String json = om.writeValueAsString(this.professional);
+            String checksum = encryptionUtils.encrypt_SHA512_base64(
+                    json + getSalt());
+            LOG.log(Level.INFO, "Checksum should be : " + checksum);
+            return this.checksum.equals(checksum);
+        } catch (Exception ex) {
+            throw new WebApplicationException(
+                    "Error validating a backup of Professional "
+                    + professional.getEmail(), ex);
+        }
+
     }
 
 }
