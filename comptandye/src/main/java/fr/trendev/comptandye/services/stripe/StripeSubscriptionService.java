363b86800a4b968580c42a420fddda5a368232f8
jsie <julien.sie@gmail.com>
2018-08-26 13:18:23 +0200

    refactor Stripe customer creation

--- comptandye/src/main/java/fr/trendev/comptandye/services/stripe/StripeSubscriptionService.java
+++ comptandye/src/main/java/fr/trendev/comptandye/services/stripe/StripeSubscriptionService.java
@@ -5,14 +5,12 @@
  */
 package fr.trendev.comptandye.services.stripe;
 
-import com.stripe.exception.StripeException;
 import com.stripe.model.Customer;
 import com.stripe.model.Source;
 import fr.trendev.comptandye.entities.Professional;
 import fr.trendev.comptandye.sessions.ProfessionalFacade;
 import fr.trendev.comptandye.utils.AuthenticationSecurityUtils;
-import java.util.HashMap;
-import java.util.Map;
+import fr.trendev.comptandye.utils.stripe.StripeCustomerUtils;
 import java.util.logging.Logger;
 import javax.annotation.security.RolesAllowed;
 import javax.ejb.Stateless;
@@ -43,6 +41,9 @@ public class StripeSubscriptionService {
     @Inject
     private AuthenticationSecurityUtils authenticationSecurityUtils;
 
+    @Inject
+    private StripeCustomerUtils stripeCustomerUtils;
+
     private final Logger LOG = Logger.getLogger(StripeSubscriptionService.class.
             getName());
 
@@ -60,52 +61,13 @@ public class StripeSubscriptionService {
             // should you the source id instead of retrieving it
             Source source = Source.retrieve(stripeSourceJson.getString("id"));
 
-            Customer customer = this.createCustomer(source, pro);
+            Customer customer = this.stripeCustomerUtils.create(source, pro);
 
             return Response.ok(customer.toJson()).build();
         } catch (Exception ex) {
             throw new WebApplicationException("Error retrieving a Stripe Source",
                     ex);
         }
-    }
-
-    private Customer createCustomer(Source source, Professional pro) throws
-            StripeException {
-        Map<String, Object> params = new HashMap<>();
-        params.put("source", source.getId());
-        params.put("email", pro.getEmail());
-        params.put("description", pro.getCltype());
-
-        Map<String, String> customerMetadata = new HashMap<>();
-        customerMetadata.put("uuid", pro.getUuid());
-        customerMetadata.put("companyName", pro.getCompanyName());
-        customerMetadata.put("companyID", pro.getCompanyID());
-        params.put("metadata", customerMetadata);
-
-        Map<String, Object> customerShipping = new HashMap<>();
-        customerShipping.put("name", pro.getCustomerDetails().getFirstName()
-                + " " + pro.getCustomerDetails().getLastName());
-        // must be international format
-        customerShipping.put("phone", pro.getCustomerDetails().getPhone());
-        Map<String, String> customerShippingAddress = new HashMap<>();
-        customerShippingAddress.put("line1", pro.getAddress().getStreet());
-        customerShippingAddress.put("city", pro.getAddress().getCity());
-        customerShippingAddress.
-                put("country", pro.getAddress().getCountry());
-        customerShippingAddress.
-                put("postal_code", pro.getAddress().getPostalCode());
-        customerShipping.put("address", customerShippingAddress);
-        params.put("shipping", customerShipping);
-
-        if (pro.getVatcode() != null && !pro.getVatcode().isEmpty()) {
-            Map<String, String> customerTaxInfo = new HashMap<>();
-            customerTaxInfo.put("type", "vat");
-            customerTaxInfo.put("tax_id", pro.getVatcode());
-            params.put("tax_info", customerTaxInfo);
-        }
-
-        return Customer.create(params);
-
     }
 
 }
---
+++ comptandye/src/main/java/fr/trendev/comptandye/utils/stripe/StripeCustomerUtils.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.trendev.comptandye.utils.stripe;
+
+import com.stripe.exception.StripeException;
+import com.stripe.model.Customer;
+import com.stripe.model.Source;
+import fr.trendev.comptandye.entities.Professional;
+import java.util.HashMap;
+import java.util.Map;
+import javax.ejb.Singleton;
+
+/**
+ *
+ * @author jsie
+ */
+@Singleton
+public class StripeCustomerUtils {
+
+    public Customer create(Source source, Professional pro) throws
+            StripeException {
+        Map<String, Object> params = new HashMap<>();
+        params.put("source", source.getId());
+        params.put("email", pro.getEmail());
+        params.put("description", pro.getCltype());
+
+        Map<String, String> customerMetadata = new HashMap<>();
+        customerMetadata.put("uuid", pro.getUuid());
+        customerMetadata.put("companyName", pro.getCompanyName());
+        customerMetadata.put("companyID", pro.getCompanyID());
+        params.put("metadata", customerMetadata);
+
+        Map<String, Object> customerShipping = new HashMap<>();
+        customerShipping.put("name", pro.getCustomerDetails().getFirstName()
+                + " " + pro.getCustomerDetails().getLastName());
+        // must be an international format
+        customerShipping.put("phone", pro.getCustomerDetails().getPhone());
+        Map<String, String> customerShippingAddress = new HashMap<>();
+        customerShippingAddress.put("line1", pro.getAddress().getStreet());
+        customerShippingAddress.put("city", pro.getAddress().getCity());
+        customerShippingAddress.
+                put("country", pro.getAddress().getCountry());
+        customerShippingAddress.
+                put("postal_code", pro.getAddress().getPostalCode());
+        customerShipping.put("address", customerShippingAddress);
+        params.put("shipping", customerShipping);
+
+        if (pro.getVatcode() != null && !pro.getVatcode().isEmpty()) {
+            Map<String, String> customerTaxInfo = new HashMap<>();
+            customerTaxInfo.put("type", "vat");
+            customerTaxInfo.put("tax_id", pro.getVatcode());
+            params.put("tax_info", customerTaxInfo);
+        }
+
+        return Customer.create(params);
+
+    }
+}
