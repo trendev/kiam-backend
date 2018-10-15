3912e8b8c3191149ec0503e80ea76ecba1f1092e
jsie <julien.sie@gmail.com>
2018-10-15 17:02:55 +0200

    @WebServlet

--- comptandye/src/main/java/fr/trendev/comptandye/utils/servlets/ReloadWebApp.java
+++ comptandye/src/main/java/fr/trendev/comptandye/utils/servlets/ReloadWebApp.java
@@ -7,6 +7,7 @@ package fr.trendev.comptandye.utils.servlets;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -15,12 +16,44 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author jsie
  */
+@WebServlet(name = "ReloadWebApp",
+        urlPatterns = {
+            "/dist/professional",
+            "/dist/individual",
+            "/dist/login",
+            "/dist/professional/*",
+            "/dist/administrator",
+            "/dist/login/*",
+            "/dist/administrator/*",
+            "/dist/individual/*",
+            "/dist/register",
+            "/dist/register/*",
+            "/dist/unsupported-user-type",
+            "/dist/unsupported-user-type/*",
+            "/dist/welcome",
+            "/dist/welcome/*",
+            "/professional",
+            "/individual",
+            "/login",
+            "/professional/*",
+            "/administrator",
+            "/login/*",
+            "/administrator/*",
+            "/individual/*",
+            "/register",
+            "/register/*",
+            "/unsupported-user-type",
+            "/unsupported-user-type/*",
+            "/welcome",
+            "/welcome/*"
+        })
 public class ReloadWebApp extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request,
             HttpServletResponse response)
             throws ServletException, IOException {
+        System.err.println("RELOADING the WEBAPP");
         request.getRequestDispatcher("/dist/index.html").include(
                 request,
                 response);
