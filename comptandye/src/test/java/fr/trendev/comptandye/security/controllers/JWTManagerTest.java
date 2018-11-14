62de42e560bf9f6e5cc55fe53623664b152cfa4c
jsie <julien.sie@gmail.com>
2018-11-14 19:01:32 +0100

    test create JWT HS256

--- comptandye/src/test/java/fr/trendev/comptandye/security/controllers/JWTManagerTest.java
+++ comptandye/src/test/java/fr/trendev/comptandye/security/controllers/JWTManagerTest.java
@@ -5,6 +5,21 @@
  */
 package fr.trendev.comptandye.security.controllers;
 
+import com.nimbusds.jose.JOSEException;
+import com.nimbusds.jose.JWSAlgorithm;
+import com.nimbusds.jose.JWSHeader;
+import com.nimbusds.jose.JWSSigner;
+import com.nimbusds.jose.KeyLengthException;
+import com.nimbusds.jose.crypto.MACSigner;
+import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.SignedJWT;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+import java.util.UUID;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -13,11 +28,67 @@ import org.junit.jupiter.api.Test;
  */
 public class JWTManagerTest {
 
+    private final String secret;
+    private final String iss;
+    private final String sub;
+    private final String aud;
+    private final Instant current_time;
+    private final Instant expiration_time;
+    private final Date iat;
+    private final Date nbf;
+    private final Date exp;
+    private final String jti;
+
     public JWTManagerTest() {
+        this.secret = "Iyi0KWCnZpwkr-HIVH1spXfSB2IiaTAk";
+        this.iss = "www.comptandye.fr";
+        this.sub = "julien.sie@gmail.com";
+        this.aud = "comptandye";
+        this.current_time = Instant.now();
+        this.expiration_time = this.current_time.plus(3,
+                ChronoUnit.MINUTES);
+        this.iat = Date.from(current_time);
+        this.nbf = Date.from(current_time);
+        this.exp = Date.from(expiration_time);
+        this.jti = UUID.randomUUID().toString();
     }
 
     @Test
-    public void testNimbus() {
+    public void testCreateJWT() {
+        try {
+            JWSSigner signer = new MACSigner(this.secret);
+
+            // Prepare JWT with claims set
+            JWTClaimsSet.Builder claimSetBuilder = new JWTClaimsSet.Builder();
+
+            claimSetBuilder.issuer(this.iss);
+            claimSetBuilder.subject(this.sub);
+            claimSetBuilder.audience(this.aud);
+            claimSetBuilder.issueTime(this.iat);
+            claimSetBuilder.notBeforeTime(this.nbf);
+            claimSetBuilder.expirationTime(this.exp);
+            claimSetBuilder.jwtID(this.jti);
+
+            JWTClaimsSet claimsSet = claimSetBuilder.build();
+
+            SignedJWT signedJWT = new SignedJWT(
+                    new JWSHeader(JWSAlgorithm.HS256), claimsSet);
+
+            // apply the HMAC protection
+            signedJWT.sign(signer);
+
+            // serialize the compact form
+            String jwt = signedJWT.serialize();
+            System.out.println(jwt);
+            Assertions.assertNotNull(jwt);
+
+        } catch (KeyLengthException ex) {
+            Logger.getLogger(JWTManagerTest.class.getName()).
+                    log(Level.SEVERE, null, ex);
+        } catch (JOSEException ex) {
+            Logger.getLogger(JWTManagerTest.class.getName()).
+                    log(Level.SEVERE, null, ex);
+        }
     }
 
 }
