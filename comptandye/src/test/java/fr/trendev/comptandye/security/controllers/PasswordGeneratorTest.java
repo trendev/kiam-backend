666c99a48b8887a2c9673cf9bc76ddeaf6985203
jsie <julien.sie@gmail.com>
2018-11-21 12:46:21 +0100

    test constructor

--- comptandye/src/test/java/fr/trendev/comptandye/security/controllers/PasswordGeneratorTest.java
+++ comptandye/src/test/java/fr/trendev/comptandye/security/controllers/PasswordGeneratorTest.java
@@ -10,6 +10,7 @@ import java.util.Set;
 import java.util.stream.IntStream;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -19,15 +20,23 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
  * @author jsie
  */
 public class PasswordGeneratorTest {
-
+    
     private PasswordManager passwordManager;
-
+    
     public PasswordGeneratorTest() {
     }
-
+    
     @Before
     public void init() {
         this.passwordManager = new PasswordManager(new HashingMechanism());
+    }
+    
+    @Test
+    public void testConstructors() {
+        PasswordManager passwordManager = new PasswordManager();
+        Assertions.assertThrows(NullPointerException.class,
+                () -> passwordManager.hashPassword("PASSWORD"));
+        
     }
 
     /**
@@ -36,26 +45,26 @@ public class PasswordGeneratorTest {
     @Test
     public void testAutoGenerate() {
         System.out.println("autoGenerate");
-
+        
         int n = 5;
         int size = 12;
         Set<String> spwd = new HashSet<>(n);
-
+        
         assertTrue(spwd.isEmpty());
-
+        
         IntStream.range(0, n).forEach(i -> spwd.add(passwordManager.
                 hashPassword(passwordManager.
                         autoGenerate(size))));
-
+        
         assertFalse(spwd.isEmpty());
-
+        
         System.out.println(n + " secured passwords generated");
         if (spwd.size() != n) {
             System.out.println(spwd.size() * 100 / n + "% are different");
         } else {
             System.out.println("They are all different");
         }
-
+        
     }
 
     /**
@@ -64,20 +73,20 @@ public class PasswordGeneratorTest {
     @Test
     public void testEncrypt_SHA256() {
         System.out.println("** encrypt_SHA256 **");
-
+        
         System.out.println("encrypt_SHA256 Base64");
         System.out.println("---------------------");
-
+        
         assertPasswordEncryption("error",
                 "ygD8z7QImJ7dxAEGLE0SGaas62ubVUEjV/F5CGLo8Xg=");
-
+        
         assertPasswordEncryption("password",
                 "XohImNooBHFR0OVvjcYpJ3NgPQ1qq73WKhHvch0VQtg=");
-
+        
         assertPasswordEncryption("comptandye_password",
                 "8oOK6BsXsv+s19XMaKMUYh6y0IvVf4Kjb7GmSWS/M0Y=");
     }
-
+    
     private void assertPasswordEncryption(String pwd, String pwd_sha256) {
         String hpwd = passwordManager.hashPassword(pwd);
         System.out.println("\"" + pwd + "\" ==> " + hpwd);
