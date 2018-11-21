fbc62d11dfad12b1cacbbc9565d29035f8031c26
jsie <julien.sie@gmail.com>
2018-11-21 15:35:25 +0100

    prepare/clean password manager tests

--- comptandye/src/test/java/fr/trendev/comptandye/security/controllers/PasswordGeneratorTest.java
+++ comptandye/src/test/java/fr/trendev/comptandye/security/controllers/PasswordGeneratorTest.java
@@ -58,12 +58,7 @@ public class PasswordGeneratorTest {
 
         assertFalse(spwd.isEmpty());
 
-        System.out.println(n + " secured passwords generated");
-        if (spwd.size() != n) {
-            System.out.println(spwd.size() * 100 / n + "% are different");
-        } else {
-            System.out.println("They are all different");
-        }
+        assertEquals(spwd.size(), n, "Passwords should be all different");
 
     }
 
@@ -77,20 +72,10 @@ public class PasswordGeneratorTest {
         System.out.println("encrypt_SHA256 Base64");
         System.out.println("---------------------");
 
-        assertPasswordEncryption("error",
-                "ygD8z7QImJ7dxAEGLE0SGaas62ubVUEjV/F5CGLo8Xg=");
+        String pwd1 = "error";
+        String pwd2 = "password";
+        String pwd3 = "comptandye_password";
 
-        assertPasswordEncryption("password",
-                "XohImNooBHFR0OVvjcYpJ3NgPQ1qq73WKhHvch0VQtg=");
-
-        assertPasswordEncryption("comptandye_password",
-                "8oOK6BsXsv+s19XMaKMUYh6y0IvVf4Kjb7GmSWS/M0Y=");
-    }
-
-    private void assertPasswordEncryption(String pwd, String pwd_sha256) {
-        String hpwd = passwordManager.hashPassword(pwd);
-        System.out.println("\"" + pwd + "\" ==> " + hpwd);
-        assertEquals(pwd_sha256, hpwd);
     }
 
     @Test
