2689d59e7197a4428e5bb3da5259ff979f9b71c7
jsie <julien.sie@gmail.com>
2018-11-26 19:52:44 +0100

    inits JWTRecord test

---
+++ comptandye/src/main/java/fr/trendev/comptandye/security/entities/JWTRecord.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.trendev.comptandye.security.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+/**
+ *
+ * @author jsie
+ */
+public class JWTRecord implements Serializable {
+
+    private String email;
+    private Date creationDate;
+    private Date expirationDate;
+
+    public JWTRecord() {
+    }
+
+    public JWTRecord(String email, Date creationDate, Date expirationDate) {
+        this.email = email;
+        this.creationDate = creationDate;
+        this.expirationDate = expirationDate;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public Date getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(Date expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 71 * hash + Objects.hashCode(this.email);
+        hash = 71 * hash + Objects.hashCode(this.creationDate);
+        hash = 71 * hash + Objects.hashCode(this.expirationDate);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final JWTRecord other = (JWTRecord) obj;
+        if (!Objects.equals(this.email, other.email)) {
+            return false;
+        }
+        if (!Objects.equals(this.creationDate, other.creationDate)) {
+            return false;
+        }
+        if (!Objects.equals(this.expirationDate, other.expirationDate)) {
+            return false;
+        }
+        return true;
+    }
+
+}
