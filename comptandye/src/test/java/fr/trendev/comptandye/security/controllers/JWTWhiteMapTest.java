5d83bc94b218913254a2c0fa4bc741ddebc2c119
jsie <julien.sie@gmail.com>
2018-11-27 19:23:18 +0100

    test add in whitemap

--- comptandye/src/test/java/fr/trendev/comptandye/security/controllers/JWTWhiteMapTest.java
+++ comptandye/src/test/java/fr/trendev/comptandye/security/controllers/JWTWhiteMapTest.java
@@ -82,28 +82,47 @@ public class JWTWhiteMapTest {
 
         String token1 = "token1";
         String token2 = "token2";
+        String token3 = "token3";
         Instant now = Instant.now();
 
         Date creationDate1 = Date.from(now);
         Date creationDate2 = Date.from(now.plus(5, MINUTES));
+        Date creationDate3 = Date.from(now.plus(10, MINUTES));
         Date expirationDate1 = Date.from(now.plus(JWTManager.VALID_PERIOD,
                 MINUTES));
         Date expirationDate2 = Date.from(now.plus(JWTManager.VALID_PERIOD + 5,
                 MINUTES));
+        Date expirationDate3 = Date.from(now.plus(JWTManager.VALID_PERIOD + 10,
+                MINUTES));
 
         JWTRecord record1 = new JWTRecord(token1, creationDate1, expirationDate1);
         JWTRecord record2 = new JWTRecord(token2, creationDate2, expirationDate2);
+        JWTRecord record3 = new JWTRecord(token3, creationDate3, expirationDate3);
 
         Map<String, Set<JWTRecord>> map = jwtwm.getMap();
         Assertions.assertNotNull(map);
         Assertions.assertTrue(map.isEmpty());
 
-        Assertions.assertNull(jwtwm.add("email1", record1));
+        Assertions.assertNull(jwtwm.add(email1, record1));
         Assertions.assertFalse(jwtwm.getMap().isEmpty());
         Assertions.assertTrue(jwtwm.getMap().size() == 1);
-        Assertions.assertNull(jwtwm.add("email2", record2));
+        Assertions.assertNull(jwtwm.add(email2, record2));
         Assertions.assertFalse(jwtwm.getMap().isEmpty());
         Assertions.assertTrue(jwtwm.getMap().size() == 2);
+
+        Set<JWTRecord> email1Records = jwtwm.add(email1, record3);
+        Assertions.assertNotNull(email1Records);
+        Assertions.assertTrue(email1Records.size() == 2);
+        Assertions.assertTrue(email1Records.contains(record1));
+        Assertions.assertTrue(email1Records.contains(record3));
+
+        email1Records.
+                add(new JWTRecord("TOKEN", creationDate1, expirationDate1));
+        Assertions.assertTrue(email1Records.size() == 3);
+
+        Assertions.assertFalse(jwtwm.getMap().isEmpty());
+        Assertions.assertTrue(jwtwm.getMap().size() == 2);
+
     }
 
 }
