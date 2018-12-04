0755d4cb1dfb64afc1616e9b58acc84c8561261f
jsie <julien.sie@gmail.com>
2018-12-04 18:40:05 +0100

    test Boolean.valueOf()

---
+++ comptandye/src/test/java/fr/trendev/comptandye/security/controllers/CustomHttpAuthenticationMechanismTest.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.trendev.comptandye.security.controllers;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+/**
+ *
+ * @author jsie
+ */
+public class CustomHttpAuthenticationMechanismTest {
+
+    public CustomHttpAuthenticationMechanismTest() {
+    }
+
+    @Test
+    public void testRememberMe() throws Exception {
+        Assertions.assertTrue(Boolean.valueOf("true"));
+        Assertions.assertFalse(Boolean.valueOf("false"));
+        Assertions.assertFalse(Boolean.valueOf("not_a_boolean"));
+        Assertions.assertFalse(Boolean.valueOf(null));
+    }
+
+}
