7f1bfcb443cc0df3f2276ee284657a196076748a
jsie <julien.sie@gmail.com>
2019-01-03 02:11:05 +0100

    inits firestore dto

---
+++ comptandye/src/test/java/fr/trendev/comptandye/security/controllers/jwt/dto/firestore/FirestoreJWTWhiteMapDTOTest.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.trendev.comptandye.security.controllers.jwt.dto.firestore;
+
+import org.junit.Test;
+
+/**
+ *
+ * @author jsie
+ */
+public class FirestoreJWTWhiteMapDTOTest {
+
+    public FirestoreJWTWhiteMapDTOTest() {
+    }
+
+    @Test
+    public void testInit() {
+    }
+
+    @Test
+    public void testClose() {
+    }
+
+}
