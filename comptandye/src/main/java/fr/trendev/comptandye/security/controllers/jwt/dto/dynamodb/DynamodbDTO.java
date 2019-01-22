04884d665462935f8173c0490a6ea9a62fe06577
Julien Sié <julien.sie@gmail.com>
2019-01-22 12:30:07 +0100

    update dto qualifiers

--- comptandye/src/main/java/fr/trendev/comptandye/security/controllers/jwt/dto/dynamodb/DynamodbDTO.java
+++ comptandye/src/main/java/fr/trendev/comptandye/security/controllers/jwt/dto/dynamodb/DynamodbDTO.java
@@ -5,10 +5,22 @@
  */
 package fr.trendev.comptandye.security.controllers.jwt.dto.dynamodb;
 
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.ElementType.TYPE;
+import java.lang.annotation.Retention;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+import java.lang.annotation.Target;
+import javax.inject.Qualifier;
+
 /**
  *
  * @author jsie
  */
+@Qualifier
+@Retention(RUNTIME)
+@Target({TYPE, METHOD, FIELD, PARAMETER})
 public @interface DynamodbDTO {
 
 }
