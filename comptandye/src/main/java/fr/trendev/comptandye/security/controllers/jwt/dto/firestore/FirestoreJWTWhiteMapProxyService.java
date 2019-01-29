71ae12279d0aa8504f306c3f5a992d34fe8b7e2e
Julien Sié <julien.sie@gmail.com>
2019-01-29 09:31:08 +0100

    import RegisterRestClient

--- comptandye/src/main/java/fr/trendev/comptandye/security/controllers/jwt/dto/firestore/FirestoreJWTWhiteMapProxyService.java
+++ comptandye/src/main/java/fr/trendev/comptandye/security/controllers/jwt/dto/firestore/FirestoreJWTWhiteMapProxyService.java
@@ -17,6 +17,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
 /**
  *
