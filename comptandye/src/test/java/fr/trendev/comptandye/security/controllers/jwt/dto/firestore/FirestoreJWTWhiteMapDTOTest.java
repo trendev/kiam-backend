fc99f720ba313401aafcd60990d6feb5b1ed897d
Julien Sié <julien.sie@gmail.com>
2019-02-13 18:39:37 +0100

    basic tests

--- comptandye/src/test/java/fr/trendev/comptandye/security/controllers/jwt/dto/firestore/FirestoreJWTWhiteMapDTOTest.java
+++ comptandye/src/test/java/fr/trendev/comptandye/security/controllers/jwt/dto/firestore/FirestoreJWTWhiteMapDTOTest.java
@@ -5,7 +5,9 @@
  */
 package fr.trendev.comptandye.security.controllers.jwt.dto.firestore;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 
 /**
  *
@@ -13,19 +15,47 @@ import org.junit.Test;
  */
 public class FirestoreJWTWhiteMapDTOTest {
 
+    private FirestoreJWTWhiteMapDTO dto;
+
     public FirestoreJWTWhiteMapDTOTest() {
+    }
+
+    @Before
+    public void init() {
+        this.dto = new FirestoreJWTWhiteMapDTO();
+        this.dto.init();
     }
 
     @Test
     public void testInit() {
+        FirestoreJWTWhiteMapDTO dto2 = new FirestoreJWTWhiteMapDTO();
+        Assertions.assertDoesNotThrow(dto2::init,
+                "DTO initialization should not throw an Exception");
     }
 
     @Test
     public void testClose() {
+        //nothing to test at the present time
     }
 
     @Test
     public void testGetAll() {
+
+//        try {
+//            CompletableFuture<List<JWTWhiteMapEntry>> asyncResponse =
+//                    this.dto.getAll()
+//                            .toCompletableFuture();
+//            List<JWTWhiteMapEntry> list = asyncResponse.get(1000,
+//                    TimeUnit.MILLISECONDS);
+//            Assertions.assertTrue(asyncResponse.isDone());
+//            Assertions.assertFalse(asyncResponse.isCompletedExceptionally());
+//        } catch (InterruptedException ex) {
+//            throw new AssertionError(ex);
+//        } catch (ExecutionException ex) {
+//            throw new AssertionError(ex);
+//        } catch (TimeoutException ex) {
+//            throw new AssertionError(ex);
+//        }
     }
 
     @Test
