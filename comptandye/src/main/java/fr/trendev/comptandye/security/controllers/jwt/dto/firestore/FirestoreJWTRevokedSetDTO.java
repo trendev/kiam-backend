fae16deaff5c175bab4e8cc3ed81f6d701862797
Julien Sié <julien.sie@gmail.com>
2019-02-20 16:50:44 +0100

    init FirestoreJWTRevokedSetDTO impl + tests

--- comptandye/src/main/java/fr/trendev/comptandye/security/controllers/jwt/dto/firestore/FirestoreJWTRevokedSetDTO.java
+++ comptandye/src/main/java/fr/trendev/comptandye/security/controllers/jwt/dto/firestore/FirestoreJWTRevokedSetDTO.java
@@ -6,11 +6,16 @@
 package fr.trendev.comptandye.security.controllers.jwt.dto.firestore;
 
 import fr.trendev.comptandye.security.controllers.jwt.dto.JWTRevokedSetDTO;
+import fr.trendev.comptandye.security.entities.JWTRecord;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.Collection;
+import java.util.List;
 import java.util.Properties;
+import java.util.Set;
+import java.util.concurrent.CompletionStage;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -69,6 +74,31 @@ public class FirestoreJWTRevokedSetDTO implements JWTRevokedSetDTO {
     public void close() {
         LOG.log(Level.INFO, "{0} closed",
                 FirestoreJWTRevokedSetDTO.class.getSimpleName());
+    }
+
+    @Override
+    public CompletionStage<Set<JWTRecord>> getAll() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void bulkRemoves(List<String> dtoRemoves) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void create(JWTRecord record) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void bulkCreation(Collection<JWTRecord> records) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void delete(String token) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
 }
