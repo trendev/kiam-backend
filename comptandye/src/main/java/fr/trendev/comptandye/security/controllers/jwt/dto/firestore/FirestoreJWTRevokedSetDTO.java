121abf6ae7bc0be1ad98ff50f4c7398a4ecfcab0
Julien Sié <julien.sie@gmail.com>
2019-02-27 19:27:44 +0100

    refactor init with the helper

--- comptandye/src/main/java/fr/trendev/comptandye/security/controllers/jwt/dto/firestore/FirestoreJWTRevokedSetDTO.java
+++ comptandye/src/main/java/fr/trendev/comptandye/security/controllers/jwt/dto/firestore/FirestoreJWTRevokedSetDTO.java
@@ -7,13 +7,9 @@ package fr.trendev.comptandye.security.controllers.jwt.dto.firestore;
 
 import fr.trendev.comptandye.security.controllers.jwt.dto.JWTRevokedSetDTO;
 import fr.trendev.comptandye.security.entities.JWTRecord;
-import java.io.IOException;
-import java.io.InputStream;
 import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.Collection;
 import java.util.List;
-import java.util.Properties;
 import java.util.Set;
 import java.util.concurrent.CompletionStage;
 import java.util.logging.Level;
@@ -35,39 +31,11 @@ public class FirestoreJWTRevokedSetDTO implements JWTRevokedSetDTO {
 
     @Override
     public void init() {
-        this.apiUri = this.loadUri();
+        this.apiUri = FirestoreJWTDTOHelper.loadUri(LOG,
+                "firestore.proxy.jwtrevokedset.url");
 
         LOG.log(Level.INFO, "{0} initialized",
                 FirestoreJWTRevokedSetDTO.class.getSimpleName());
-    }
-
-    private URI loadUri() {
-
-        ClassLoader classloader = Thread.currentThread().
-                getContextClassLoader();
-
-        try (InputStream is = classloader.getResourceAsStream(
-                "firestore/firestore.properties")) {
-
-            Properties properties = new Properties();
-            properties.load(is);
-
-            // loads the properties
-            String url = properties.getProperty(
-                    "firestore.proxy.jwtrevokedset.url");
-
-            LOG.log(Level.INFO,
-                    "firestore.proxy.jwtrevokedset.url = \"{0}\"",
-                    url);
-
-            return new URI(url);
-        } catch (URISyntaxException ex) {
-            throw new IllegalStateException(
-                    "Url provided in properties is not valid", ex);
-        } catch (IOException ex) {
-            throw new IllegalStateException(
-                    "IO Errors setting Firestore properties", ex);
-        }
     }
 
     @Override
