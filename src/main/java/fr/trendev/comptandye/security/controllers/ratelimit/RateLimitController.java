0f1cbaa7ee2f0cd4d9d3c46c1032625310cb7d3b
Julien Sié <julien.sie@gmail.com>
2019-08-07 19:59:16 +0200

    implements/test rate limit per path

--- src/main/java/fr/trendev/comptandye/security/controllers/ratelimit/RateLimitController.java
+++ src/main/java/fr/trendev/comptandye/security/controllers/ratelimit/RateLimitController.java
@@ -7,14 +7,28 @@ package fr.trendev.comptandye.security.controllers.ratelimit;
 
 import fish.payara.cluster.Clustered;
 import fish.payara.cluster.DistributedLockType;
+import java.io.FileNotFoundException;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.TreeMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.ejb.Schedule;
+import javax.ejb.Schedules;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
 
 /**
  *
@@ -30,22 +44,112 @@ public class RateLimitController implements Serializable {
 
     private final String className = RateLimitController.class.getSimpleName();
 
+    private static volatile Map<String, Integer> PATH_LIMITATIONS
+            = Collections.synchronizedSortedMap(new TreeMap<>());
+
+    private static volatile Map<String, Map<String, List<Date>>> ACCESS_RECORDS
+            = Collections.synchronizedSortedMap(new TreeMap<>());
+
+    private static final int DEFAULT_LIMIT = 10;
+
+    private static final int NO_LIMIT = -1;
+
     public RateLimitController() {
     }
 
     @PostConstruct
     public void init() {
-        LOG.log(Level.INFO, "{0} : initialized", className);
+        try {
+            this.loadLimitations();
+            LOG.log(Level.INFO, "{0} : initialized", className);
+            this.logPathLimitations();
+        } catch (FileNotFoundException ex) {
+            LOG.log(Level.SEVERE, "Error initializing " + className, ex);
+        }
+
     }
 
     @PreDestroy
     public void destroy() {
+        PATH_LIMITATIONS.clear();
         LOG.log(Level.INFO, "{0} : destroyed", className);
     }
-    
-    public Optional<Boolean> control(String path){
-        return Optional.empty();
+
+    private void loadLimitations() throws FileNotFoundException {
+        JsonReader reader = Json.createReader(
+                Thread.currentThread().getContextClassLoader()
+                        .getResourceAsStream("json/path_limitations.json"));
+        JsonArray ja = reader.readArray();
+        PATH_LIMITATIONS.clear();
+        ja.forEach(jv -> {
+            JsonObject jo = jv.asJsonObject();
+            PATH_LIMITATIONS.put(
+                    jo.getString("path"),
+                    jo.getInt("limit"));
+        });
     }
 
+    private void logPathLimitations() {
+        PATH_LIMITATIONS.forEach(
+                (path, limit) -> LOG.log(Level.INFO,
+                        "## Path : {0} | Limit : {1} ##",
+                        new Object[]{path, limit})
+        );
+    }
+
+    private Optional<Integer> controlPath(String path) {
+        return PATH_LIMITATIONS.containsKey(path)
+                ? Optional.of(PATH_LIMITATIONS.get(path))
+                : Optional.empty();
+    }
+
+    private Optional<Boolean> controlAccess(String address,
+            String path,
+            int limit) {
+        if (limit == NO_LIMIT) {
+            return Optional.empty();
+        } else {
+            return Optional.ofNullable(ACCESS_RECORDS.get(address))
+                    .map(
+                            m -> Optional.ofNullable(m.get(path))
+                                    .map(list -> {
+                                        if (list.size() < limit) {
+                                            list.add(new Date());
+                                            return Optional.<Boolean>empty();
+                                        } else {
+                                            return Optional.of(Boolean.TRUE);
+                                        }
+                                    })
+                                    .orElseGet(() -> {
+                                        List<Date> list = new LinkedList<>();
+                                        list.add(new Date());
+                                        m.put(path, list);
+                                        return Optional.<Boolean>empty();
+                                    })
+                    )
+                    .orElseGet(() -> {
+                        List<Date> list = new LinkedList<>();
+                        list.add(new Date());
+                        Map<String, List<Date>> map = new TreeMap<>();
+                        map.put(path, list);
+                        ACCESS_RECORDS.put(address, map);
+                        return Optional.<Boolean>empty();
+                    });
+        }
+    }
+
+    public Optional<Boolean> control(String address, String path) {
+        return this.controlPath(path)
+                .map(limit -> this.controlAccess(address, path, limit))
+                .orElseGet(() -> this.controlAccess(address, path, DEFAULT_LIMIT));
+    }
+
+    @Schedules({
+        @Schedule(minute = "*", hour = "*")
+    })
+    void reset() {
+        ACCESS_RECORDS.clear();
+        LOG.info("ACCESS_RECORDS has been reset");
+    }
 
 }
--- src/main/java/fr/trendev/comptandye/security/filters/RateLimitFilter.java
+++ src/main/java/fr/trendev/comptandye/security/filters/RateLimitFilter.java
@@ -5,16 +5,22 @@
  */
 package fr.trendev.comptandye.security.filters;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import fr.trendev.comptandye.security.controllers.ratelimit.RateLimitController;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
+import javax.json.Json;
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.container.PreMatching;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.Provider;
 
@@ -26,27 +32,37 @@ import javax.ws.rs.ext.Provider;
 @Provider
 @RequestScoped
 public class RateLimitFilter implements ContainerRequestFilter {
-    
+
     private static final Logger LOG = Logger.getLogger(RateLimitFilter.class.getName());
-    
+
     private final String className = RateLimitFilter.class.getSimpleName();
-    
+
     @Context
     private HttpServletRequest req;
-    
+
     @Inject
     private RateLimitController arc;
-    
+  
     @Override
     public void filter(ContainerRequestContext cr) {
-        if (arc.control(req.getRemoteAddr(),
-                cr.getUriInfo().getPath()).isPresent()) {
-            LOG.log(Level.WARNING, "{1} : RemoteAddr {0} >>> TOO_MANY_REQUESTS",
-                new Object[]{
-                    req.getRemoteAddr(),
-                    className});
-            cr.abortWith(Response.status(Response.Status.TOO_MANY_REQUESTS).build());
+
+        Optional<Boolean> result = arc.control(req.getRemoteAddr(),
+                cr.getUriInfo().getPath());
+
+        if (result.isPresent()) {
+            LOG.log(Level.WARNING, "{1} : RemoteAddr {0} >>> TOO_MANY_REQUESTS {2}/{2}",
+                    new Object[]{
+                        req.getRemoteAddr(),
+                        className,
+                        result.get()});
+            cr.abortWith(Response
+                    .status(Response.Status.TOO_MANY_REQUESTS)
+                    .type(MediaType.APPLICATION_JSON)
+                    .entity(Json.createObjectBuilder()
+                            .add("error", "TOO_MANY_REQUESTS")
+                            .build())
+                    .build());
         }
     }
-    
+
 }
