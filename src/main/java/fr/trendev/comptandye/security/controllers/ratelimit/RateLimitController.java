c97f852cce1e81833f9e2638b8b74ef4dfe9cf8b
Julien Sié <julien.sie@gmail.com>
2019-08-08 17:13:36 +0200

    improve rate limit filter tuning

--- src/main/java/fr/trendev/comptandye/security/controllers/ratelimit/RateLimitController.java
+++ src/main/java/fr/trendev/comptandye/security/controllers/ratelimit/RateLimitController.java
@@ -9,7 +9,6 @@ import fish.payara.cluster.Clustered;
 import fish.payara.cluster.DistributedLockType;
 import java.io.FileNotFoundException;
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Date;
 import java.util.LinkedList;
@@ -39,24 +38,24 @@ import javax.json.JsonReader;
 @Singleton
 @Startup
 public class RateLimitController implements Serializable {
-
+    
     private static final Logger LOG = Logger.getLogger(RateLimitController.class.getName());
-
+    
     private final String className = RateLimitController.class.getSimpleName();
-
+    
     private static volatile Map<String, Integer> PATH_LIMITATIONS
             = Collections.synchronizedSortedMap(new TreeMap<>());
-
+    
     private static volatile Map<String, Map<String, List<Date>>> ACCESS_RECORDS
             = Collections.synchronizedSortedMap(new TreeMap<>());
-
-    private static final int DEFAULT_LIMIT = 10;
-
+    
+    private static volatile int defaultLimit;
+    
     private static final int NO_LIMIT = -1;
-
+    
     public RateLimitController() {
     }
-
+    
     @PostConstruct
     public void init() {
         try {
@@ -66,20 +65,26 @@ public class RateLimitController implements Serializable {
         } catch (FileNotFoundException ex) {
             LOG.log(Level.SEVERE, "Error initializing " + className, ex);
         }
-
+        
     }
-
+    
     @PreDestroy
     public void destroy() {
         PATH_LIMITATIONS.clear();
         LOG.log(Level.INFO, "{0} : destroyed", className);
     }
-
+    
     private void loadLimitations() throws FileNotFoundException {
         JsonReader reader = Json.createReader(
                 Thread.currentThread().getContextClassLoader()
                         .getResourceAsStream("json/path_limitations.json"));
-        JsonArray ja = reader.readArray();
+        
+        JsonObject config = reader.readObject();
+        
+        defaultLimit = config.getInt("default");
+        
+        JsonArray ja = config.getJsonArray("limitations");
+        
         PATH_LIMITATIONS.clear();
         ja.forEach(jv -> {
             JsonObject jo = jv.asJsonObject();
@@ -88,22 +93,23 @@ public class RateLimitController implements Serializable {
                     jo.getInt("limit"));
         });
     }
-
+    
     private void logPathLimitations() {
+        LOG.log(Level.INFO, "## DEFAULT LIMIT = {0}", defaultLimit);
         PATH_LIMITATIONS.forEach(
                 (path, limit) -> LOG.log(Level.INFO,
                         "## Path : {0} | Limit : {1} ##",
                         new Object[]{path, limit})
         );
     }
-
+    
     private Optional<Integer> controlPath(String path) {
         return PATH_LIMITATIONS.containsKey(path)
                 ? Optional.of(PATH_LIMITATIONS.get(path))
                 : Optional.empty();
     }
-
-    private Optional<Map<String,List<Date>>> controlAccess(String address,
+    
+    private Optional<Map<String, List<Date>>> controlAccess(String address,
             String path,
             int limit) {
         if (limit == NO_LIMIT) {
@@ -115,7 +121,7 @@ public class RateLimitController implements Serializable {
                                     .map(list -> {
                                         if (list.size() < limit) {
                                             list.add(new Date());
-                                            return Optional.<Map<String,List<Date>>>empty();
+                                            return Optional.<Map<String, List<Date>>>empty();
                                         } else {
                                             return Optional.of(records);
                                         }
@@ -124,7 +130,7 @@ public class RateLimitController implements Serializable {
                                         List<Date> list = new LinkedList<>();
                                         list.add(new Date());
                                         records.put(path, list);
-                                        return Optional.<Map<String,List<Date>>>empty();
+                                        return Optional.<Map<String, List<Date>>>empty();
                                     })
                     )
                     .orElseGet(() -> {
@@ -133,17 +139,17 @@ public class RateLimitController implements Serializable {
                         Map<String, List<Date>> map = new TreeMap<>();
                         map.put(path, list);
                         ACCESS_RECORDS.put(address, map);
-                        return Optional.<Map<String,List<Date>>>empty();
+                        return Optional.<Map<String, List<Date>>>empty();
                     });
         }
     }
-
-    public Optional<Map<String,List<Date>>> control(String address, String path) {
+    
+    public Optional<Map<String, List<Date>>> control(String address, String path) {
         return this.controlPath(path)
                 .map(limit -> this.controlAccess(address, path, limit))
-                .orElseGet(() -> this.controlAccess(address, path, DEFAULT_LIMIT));
+                .orElseGet(() -> this.controlAccess(address, path, defaultLimit));
     }
-
+    
     @Schedules({
         @Schedule(minute = "*", hour = "*")
     })
@@ -151,5 +157,5 @@ public class RateLimitController implements Serializable {
         ACCESS_RECORDS.clear();
         LOG.info("ACCESS_RECORDS has been reset");
     }
-
+    
 }
