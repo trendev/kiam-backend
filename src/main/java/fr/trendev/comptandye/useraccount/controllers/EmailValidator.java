64f2c2b84707e6159cbfb43c007ce44476bf8a40
Julien Sié <julien.sie@gmail.com>
2019-08-16 16:58:56 +0200

    implement/test email syntax validator

---
+++ src/main/java/fr/trendev/comptandye/useraccount/controllers/EmailValidator.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.trendev.comptandye.useraccount.controllers;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author jsie
+ */
+@Stateless
+public class EmailValidator {
+    
+    private final static String REGEX = "^[\\w!#$%&'*+/=?`{|}~^-]+(?:\\.[\\w!#$%&'*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$";
+    
+    private final Pattern pattern;
+
+    public EmailValidator() {
+        this.pattern = Pattern.compile(REGEX);
+    }
+    
+    public boolean valid(String email){
+        
+        if(email == null ||  email.isEmpty()){
+            return false;
+        }
+        
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+    
+}
---
+++ src/test/java/fr/trendev/comptandye/useraccount/controllers/EmailValidatorTest.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.trendev.comptandye.useraccount.controllers;
+
+import java.util.Arrays;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+/**
+ *
+ * @author jsie
+ */
+public class EmailValidatorTest {
+
+    private final EmailValidator validator;
+
+    public EmailValidatorTest() {
+        this.validator = new EmailValidator();
+    }
+
+    @Test
+    public void testValid() {
+
+        Arrays.asList(
+                "user@domain.com",
+                "user@domain.co.in",
+                "user.name@domain.com",
+                "user_name@domain.com",
+                "username@yahoo.corporate.in",
+                "user?name@domain.co.in",
+                "name.name@name-names.domain.no",
+                "firstname-lastname@name2.names.domain.no"
+        ).forEach(e
+                -> Assertions.assertTrue(validator.valid(e),
+                        e + " should be valid"));
+
+        Arrays.asList(
+                ".username@yahoo.com",
+                "username@yahoo.com.",
+                "username@yahoo..com",
+                "username@yahoo.c",
+                "username@yahoo.corporate"
+        ).forEach(e
+                -> Assertions.assertFalse(validator.valid(e),
+                        e + " should not be valid"));
+    }
+
+}
