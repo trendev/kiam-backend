de530124a8c5a76ec64515cbeac9047e1937a516
Julien Sié <julien.sie@gmail.com>
2021-10-12 16:46:51 +0200

    clean imports

--- src/main/java/fr/trendev/kiam/security/controllers/RSAKeyProvider.java
+++ src/main/java/fr/trendev/kiam/security/controllers/RSAKeyProvider.java
@@ -22,10 +22,8 @@ import java.util.logging.Logger;
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Produces;
-import javax.inject.Inject;
 import org.eclipse.microprofile.config.Config;
 import org.eclipse.microprofile.config.ConfigProvider;
-import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 /**
  *
